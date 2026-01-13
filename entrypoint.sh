805bfff2e73361d84014a1d2d729667ed5f98a5c
Trung Le <jackblk4@gmail.com>
2026-01-13 11:52:34 +0700

    fix: add args to prevent X server disconnects

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 # Start Xvfb in background
 echo "Starting Xvfb..."
-Xvfb :99 -screen 0 1280x720x24 &
+
+# Virtual display :99, resolution 1280x720, 24-bit color
+# Disable TCP X11 connections, avoid network-related resets
+# Do NOT reset X server when last client disconnects (critical)
+# Disable access control, allow reconnects from same container
+Xvfb :99 \
+  -screen 0 1280x720x24 \
+  -nolisten tcp \
+  -noreset \
+  -ac &
+
 export DISPLAY=:99
 
 # Execute whatever CMD is passed
